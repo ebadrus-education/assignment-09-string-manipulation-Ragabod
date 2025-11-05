079e6c3261f1dbb53bb2dc614f067b111c6053cc
Raga Syahputra Arifin <ragasyahputra30@gmail.com>
2025-11-05 13:56:31 +0700

    Tambah 04 find

---
+++ 04.find/find.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama1 = "DwiAnanda";
+    int posisi = nama1.find("An");
+    cout << "Posisi 'An': " << posisi << endl;
+    return 0;
+}
