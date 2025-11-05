2148b5079ee4a7d17025c95efee75c79edd7410f
Raga Syahputra Arifin <ragasyahputra30@gmail.com>
2025-11-05 13:48:40 +0700

    Tambah 01.length.size

---
+++ 01.length.size/length_size.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama1 = "DwiAnanda";
+    cout << "Panjang string: " << nama1.length() << endl;
+    cout << "Panjang string (pakai size): " << nama1.size() << endl;
+    return 0;
+}
